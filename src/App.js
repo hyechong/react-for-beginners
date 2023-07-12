a06e65a3c98bac9165c8f96a8b1636a3894bb785
hyechong <ckgpwl2@gmail.com>
2023-07-12 15:46:58 +0900

    0712

--- src/App.js
+++ src/App.js
@@ -4,26 +4,52 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const [showing, setShowing] = useState(false);
+  // const onClick = () => {
+  //   setValue((prev) => prev + 1);
+  // };
+  function Hello() {
+    useEffect(() => {
+      console.log('hi :)');
+      return () => console.log('bye :(');
+    }, []);
+    return <h1>Hello!</h1>;
+  }
   const onClick = () => {
-    setValue((prev) => prev + 1);
+    setShowing((prev) => !prev);
   };
-  console.log('render');
-  const iRunOnlyOnce = () => {
-    console.log('i run only once');
+  const onChange = (event) => {
+    setKeyword(event.target.value);
   };
-  useEffect(iRunOnlyOnce, []); // 첫 번째 render 시에만 함수 실행
-  // useEffect
-  // - 두 개의 argument를 가지는 함수
-  // - 첫 번째 argument는 우리가 딱 한번만 실행하고 싶은 코드
-  // - 두 번째는 [] 배열을 넣어줌
-  // -> useEffect가 컴포넌트의 첫 번째 렌더 시점에 iRunOnlyOnce 함수 호출
-  // 그리고 상태를 변화시키면 iRunOnlyOnce는 호출되지 않음
-  // 즉, 한번만 렌더링 됨
+  useEffect(() => {
+    console.log('movie api');
+  }, []);
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 1) {
+      console.log('search for', keyword);
+    }
+  }, [keyword]); // [dependency(지켜보려는것)] : dependency가 변화 할 때 코드를 실행, 따라서 [](빈 배열)로 두면 처음 한 번만 실행되는 것임 cf. [keyword, counter]와 같은 형태로 적어 keyword나 counter 둘 중 하나가 변화할 때 코드 실행시킬 수도 있음
+  // useEffect(() => {}, [counter]);
+  // console.log('i run all the time');
+  // const iRunOnlyOnce = () => {
+  //   console.log('i run only once');
+  // };
+  // useEffect(iRunOnlyOnce, []); // 첫 번째 render 시에만 함수 실행
+  // useEffect를 통해 언제 코드를 실행할 지 선택권을 가질 수 있음
   // 단순화 하여 useEffect(() => {console.log("CALL THE API")},[]); 써도 됨
   return (
     <div className='App'>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type='text'
+        placeholder='Search here...'
+      />
       <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {/* <button onClick={onClick}>click me</button> */}
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
