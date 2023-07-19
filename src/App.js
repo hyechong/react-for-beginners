6d1e9740f29c8794b0175318b35b84fc094c8707
hyechong <ckgpwl2@gmail.com>
2023-07-19 22:40:02 +0900

    movie app start 0719

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import Button from './Button';
-import styles from './App.module.css';
 import { useState, useEffect } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
+    fetch(`https://yts.mx/api
+    https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
       .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+      .then((json) => console.log(json));
   }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
